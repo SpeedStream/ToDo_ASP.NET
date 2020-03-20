72480c947b583ce012b663af7d9b75d5b431bf5b
SpeedStream <aesc91@hotmail.com>
2020-03-19 22:18:40 -0600

    Model classes Category and Task added

---
+++ Models/Category.vb
@@ -0,0 +1,19 @@
+﻿Imports System.ComponentModel.DataAnnotations
+Public Class Category
+    Private Sub Category()
+    End Sub
+
+    Public Property Id As Integer
+
+    <Required(ErrorMessage:="Category is required")>
+    Public Property Name As String
+
+    Public Sub Category(name As String)
+        SetName(name)
+    End Sub
+
+    Public Sub SetName(NameVal As String)
+        Name = Name
+    End Sub
+
+End Class
---
+++ Models/Task.vb
@@ -0,0 +1,49 @@
+﻿Imports System.ComponentModel.DataAnnotations
+Public Class Task
+    Private _description As String
+    Private _isComplete As String = False
+    Private Sub Task()
+    End Sub
+
+    Public Sub Task(description As String, category As Category)
+        setDetails(description, category)
+    End Sub
+
+    Public Property Id As Integer
+
+    <Required(ErrorMessage:="Description is required")>
+    Public Property Description As String
+        Get
+            Return _description
+        End Get
+        Private Set(value As String)
+            _description = value
+        End Set
+    End Property
+
+    Public Property IsComplete As Boolean
+        Get
+            Return _isComplete
+        End Get
+        Private Set(value As Boolean)
+            _isComplete = value
+        End Set
+    End Property
+
+    <Required(ErrorMessage:="Category is required")>
+    Public Property Category As Category
+
+    Public Sub setDetails(description As String, category As Category)
+        Me.Description = description
+        Me.Category = category
+    End Sub
+
+    Public Sub MarkComplete()
+        IsComplete = True
+    End Sub
+
+    Public Sub MarkIncomplete()
+        IsComplete = False
+    End Sub
+
+End Class
